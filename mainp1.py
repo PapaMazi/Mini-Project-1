7f3ba2037ed97a75a6595f70131709b195d2b5b1
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-10-25 18:28:00 -0600

    added some skeleton code to accept any database name, added a connect_db function to establish a connection between db and client

--- mainp1.py
+++ mainp1.py
@@ -1,3 +1,37 @@
+import sqlite3
+import time
+from os.path import isfile, getsize
+conn = None
+cursor = None
+
+
+
+def connect_db(dbname):
+    global conn, cursor
+    if not isfile(dbname):
+        return False
+    conn = sqlite3.connect(dbname)
+    cursor = conn.cursor()
+    cursor.execute(' PRAGMA foreign_keys=ON; ')
+    conn.commit()
+    return True
+    
+
 
 def main():
-    #add code
+    exit_condition = True
+    print("Welcome to this interface, here you can interact with our database system,\nplease enter your sqlite database path to continue:")
+    dbname = input()
+    while exit_condition:
+        connectCheck = connect_db(dbname)
+        if connectCheck == True:
+            print("Database connected succesfully!")
+            exit_condition = False
+        else:
+            print("an error occured! please try again:")
+            dbname = input()
+            continue
+            
+            
+if __name__ == "__main__":
+    main()
