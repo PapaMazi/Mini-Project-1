812b9571ab02397e8dbe4eaf13917b3fbbbd98ef
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-10-25 20:46:42 -0600

    fixed a couple of issues, improved useability slightly

--- mainp1.py
+++ mainp1.py
@@ -15,21 +15,26 @@ def connect_db(dbname):
     conn.commit()
     return True
 
-def login_memu():
+def login_menu():
+    login_condition = True
     operations = ["1. Login", "2. Register"]
-    for i in range(len(operations)):
-        print(operations[i])
-    oper = input("Please select operation or enter 0 to exit: ")
-    if oper == "1":
-        user = input("Enter Username: ")
-        passw = input("Enter Password")  # NEED TO HIDE THIS. DO LATER
-        signin(user, passw)
-    elif oper == "2":
-        register()
-    elif oper == "0":
-        quit()
-    else:
-        oper = input("Incorrect input. Please select operation or enter 0 to exit: ")
+    while login_condition:
+        for i in range(len(operations)):
+            print(operations[i])
+        oper = input("Please select operation or enter 0 to exit: ")
+        if oper == "1":
+            user = input("Enter Username: ")
+            passw = input("Enter Password: ")  # NEED TO HIDE THIS. DO LATER
+            signin(user, passw)
+            login_condition = False
+        elif oper == "2":
+            register()
+            login_condition = False
+        elif oper == "0":
+            quit()
+        else:
+            oper = input("Incorrect input. Please select operation or enter 0 to exit: ")
+            continue
 
 def signin(user, passw):  # Handle user signin here
     pass
@@ -42,19 +47,18 @@ def main():
     exit_condition = True
 
     dbname = input("Welcome to this interface, here you can interact with our database system,\nplease enter your sqlite database path to continue: ")
-
-    print("Welcome to this interface, here you can interact with our database system,\nplease enter your sqlite database path to continue:")
-    dbname = input()
     while exit_condition:
         connectCheck = connect_db(dbname)
         if connectCheck == True:
             print("Database connected succesfully!")
             exit_condition = False
-            login_memu()
+            
         else:
             print("an error occured! please try again:")
             dbname = input()
             continue
+
+    login_menu()
 
 if __name__ == "__main__":
     main()
